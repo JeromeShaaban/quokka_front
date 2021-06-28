026f33459d2a9e4829332524bab4259df1ffcf54
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-06-29 00:48:12 +0200

    3 recipes created and translated + add button fav but not finish yet

--- src/components/recipe/Card.jsx
+++ src/components/recipe/Card.jsx
@@ -1,40 +1,71 @@
 import styled from 'styled-components';
+import {useState} from 'react';
 
 export const StyledImageRecipe = styled.img`
-  height: 440px;
-  width: 600px;
+  height: 340px;
+  width: 500px;
 `;
 
 export const StepsRecipe = styled.p`
  font-family:'Roboto', sans-serif;
+ text-align:justify;
+ width:30rem;
+ color:#fff;
 `;
 
 
 export const RecipeTitle = styled.h2`
  font-size:25px;
+ color:#fff;
 `;
 
 export const IngredientsRecipe = styled.p`
- font-size:14px;
-  width: 300px;
+font-size: 1.1rem;
+padding: 5px;
+text-align:justify;
+color:#fff;
 `;
 
-export const CardRecipeContainer = styled.p`
- background-color: #535353;
-  width: 700px;
+export const CardRecipeContainer = styled.div`
+background-color: #ffffff18;
+  width: 500px;
+  padding:40px;
+  border-radius:5%;
+  margin:40px;
+
+
+h4{
+    color:#ffffffea;
+    font-size:1.4rem;
+}
 `;
 
+export const ButtonFavRecipe = styled.button`
+padding: 10px;
+margin-top: 30px;
+`;
 
+export default function Card({title, image, ingredients, steps1, steps2, steps3, steps4, steps5}){
+    const [isFavorite, setFavorite] = useState()
 
-export default function Card({title, image, ingredients, steps1, steps2, steps3}){
-return(
+    const HandleClick = () =>{
+        setFavorite(!isFavorite);
+
+    }
+
+    return(
     <CardRecipeContainer>
     <RecipeTitle>{title}</RecipeTitle>
     <StyledImageRecipe src={image} alt={title} />
+    <h4>ingredients</h4>
     <IngredientsRecipe>{ingredients}</IngredientsRecipe>
+    <h4>Steps: </h4>
     <StepsRecipe>{steps1}</StepsRecipe>
     <StepsRecipe>{steps2}</StepsRecipe>
     <StepsRecipe>{steps3}</StepsRecipe>
+    <StepsRecipe>{steps4}</StepsRecipe>
+    <StepsRecipe>{steps5}</StepsRecipe>
+    <ButtonFavRecipe onClick={HandleClick} class={'isfavorite' ? 'isfavorite' : 'notFavorite'}>add to favorite : ❤️</ButtonFavRecipe >
     </CardRecipeContainer>
 )
 }
--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -5,6 +5,22 @@ import BackgroundStars from '../BackgroundStars';
 
 export const RecipeWrapper = styled.div`
 display:flex;
+flex-direction:row;
+justify-content: center;
+  flex-wrap:wrap;
+`;
+
+
+export const TitleRecipePage = styled.h1`
+margin:0;
+color:#fff;
+padding:50px;
+margin-left: auto;
+margin-right: auto;
+width: 16em;
+border-radius: 10%;
+font-size: 4rem;
+
 `;
 
 export default function Recipe(){
@@ -12,34 +28,52 @@ export default function Recipe(){
     const recipes = [
         {
            
-            title:'Tarte flambée saumon ',
-            image:'https://cdn.pixabay.com/photo/2018/05/11/18/07/tarte-flambee-3391213_960_720.jpg',
-            ingredients:'1Pâte à Flammekueche(ou à défaut une pâte à pizza), 200g Fromage blanc 0%, 5cuil. à soupe Crème fraîche, 200g Oignons émincés, 150g Saumon fumé, poivre du moulin ',
-            steps1:' 1. Couper le saumon en lamelles.',
-            steps2:'2. Dans un bol, mélanger le fromage blanc, la crème fraiche et le poivre.',
-            steps3:'2. Dans un bol, mélanger le fromage blanc, la crème fraiche et le poivre.'
+            title:'Salmon pie',
+            image:'https://cdn.pixabay.com/photo/2020/03/03/16/18/tarte-flambee-4899104_960_720.jpg',
+            ingredients:'1 flamenkuche paste(or a pizza paste),200g White cheese 0%, 5tbsp. fresh cream, 200g sliced ​​onions, 150g smoked salmon, freshly ground pepper ',
+            steps1:" 1. 🔪 🐟 Cut the salmon into strips",
+            steps2:'2. 🥣 In a bowl, combine the white cheese, sour cream and pepper.',
+            steps3:'3. 🫓 Spread the dough on a baking sheet. Spread the cream mixture on top and distribute the onions and the slices of salmon.',
+            steps4: '4. ✨ Bake at 220 ° C for 15 minutes',
+            isfavorite:'false'
         },
         {
             
-            title:'Salade grec',
+            title:'Greek salad',
             image:'https://images.pexels.com/photos/434258/pexels-photo-434258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-            ingredients:"200g Feta, 3 Tomates 2 Poivrons vert 2 échalotes100g Olives noires Huile d'olive 1 cuil. à café Vinaigre balsamique 1 cuil. à café Herbes de provence(mélange du commerce)",
-            steps:'faire ceci et faire cela'
+            ingredients:"200g Feta, 3 Tomatoes, 2 Green peppers, 2 shallots, 100g black olives, Olive oil 1 tsp. teaspoon Balsamic vinegar, 1 tsp. 1 teaspoon of Provencal herbs (store-bought mix)",
+            steps1:' 1. 🔪 🍅 Cut the tomatoes into small cubes and put them in a salad bowl.',
+            steps2:'2. 🥒 🫑 🫒 Add the cucumber,green pepper and olives.',
+            steps3:"3. 🧅 Mix with half of the onion and diced feta",
+            steps4:"4. 🍋 Add the lemon zest and juice, season with oil and oregano, salt and pepper.",
+            steps5:"5. 🥗 Mix gently and serve.",
+            isfavorite:'false'
+        },
+        {
+            
+            title:'Vegetarian burger',
+            image:'https://cdn.pixabay.com/photo/2017/08/04/11/35/burger-2579902_960_720.jpg',
+            ingredients:"4 salad leaves, 4 slices toastinette, hamburger buns, tomatoes slices, pickles, mustard, corn (200 g), peas (200 g), spices (salt, pepper, paprika, cumin, curry ...) one egg",
+            steps1:' 1. 🌽 🥚 🧅 Blend the peas, corn, onion (cut into 4) and egg in a blender.',
+            steps2:"2. 🫓 🧂 🌶️ Then add the spices, flour and breadcrumbs to the mixture until you have a thick dough (so you can form a crepe",
+            steps3:"3. 🍳 Form crepes in a hot pan with a little oil. Leave to cook for about 15 minutes over medium heat. Before removing them from the heat, add the cheese slices so that they melt a little. ",
+            steps4:"4. 🍅 🫓 🥬 You can then compose your burgers with bread, salads, tomatoes slices, sauces and pancake. ",
+            steps5:"5. 🍔 It's ready! Bon appétit !",
+            isfavorite:'false'
         },
     ]
 
     return(
-        <RecipeWrapper>
+        <div>
             <BackgroundStars/>
-            <h1>blablabla</h1>
-            <div>
+            <TitleRecipePage>A healthy mind in a healthy body</TitleRecipePage>
+            <RecipeWrapper>
         {recipes.map((recipe) => {
             return(
             <Card {...recipe}/>
         )})}
-        </div>
-        <p>gnagnagna</p>
         </RecipeWrapper>
+        </div>
 
     )
 }
