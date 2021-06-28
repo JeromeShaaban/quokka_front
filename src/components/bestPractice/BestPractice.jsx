e7175b1533558a7a9fcab73822ec320e359a5059
SophieTopart <sophiebailly9@gmail.com>
2021-06-28 21:12:35 +0200

    Blur

---
+++ src/components/bestPractice/BestPractice.jsx
@@ -0,0 +1,12 @@
+import BackgroundStars from '../BackgroundStars'
+import Card from './Card'
+
+export default function BestPractice () {
+
+    return(
+    <div>
+        <BackgroundStars />
+        <Card />
+    </div>
+    )
+  }
---
+++ src/components/bestPractice/Card.jsx
@@ -0,0 +1,11 @@
+import '../../style/cards.scss'
+
+export default function Card () {
+
+    return(
+        <>
+        <h1 id="white">Hi</h1>
+        <div id="blur"></div>
+    </>
+    )
+  }
