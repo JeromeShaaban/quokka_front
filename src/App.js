763233fa2d58eac6885ea02189aaf9da35843790
Pierre <lightpower@hotmail.fr>
2021-06-28 16:48:19 +0200

    fix to pos absolute the background & imported icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import BackgroundStars from './components/BackgroundStars';
+import Loader from './components/loader/Loader';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BackgroundStars/>
+        <Loader/>
       </header>
     </div>
   );
