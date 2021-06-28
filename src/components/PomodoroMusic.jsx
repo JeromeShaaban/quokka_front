dbba49d8ed6f977ca8a7a089161f0e9d03395fd5
JaddN7 <jerome.shaaban@gmail.com>
2021-06-28 19:34:04 +0200

    Testing possibilities for the logo

--- src/components/PomodoroMusic.jsx
+++ src/components/PomodoroMusic.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../style/pomodoroMusic.css';
 import '../style/backgroundStars.scss';
+import tomato_shape from '../assets/25minwhite.png';
 
 const PomodoroMusic = () => {
   return (
     <div className="bkg">
-        <h1 id="title">25</h1>
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-        <div id="glows"></div>
+      <h1 id="title">25</h1>
+      <img src={tomato_shape} alt="tomato_shape" style={{width: "14%", height: "30%"}}/>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="glows"></div>
     </div>
   );
 };
