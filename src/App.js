f2da5dbf5f21e2e63107b64f4dde25f47b0b7637
SophieTopart <sophiebailly9@gmail.com>
2021-06-29 12:14:57 +0200

    Correc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import QuokkaRouter from "./Router";
 import './App.css';
+import MenuContext from './contexts/MenuContext'
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
+    <MenuContext.Provider value={{ open: open, setOpen: setOpen}}>
     <div className="App">
       <QuokkaRouter />
     </div>
+    </MenuContext.Provider>
   );
 }
 
