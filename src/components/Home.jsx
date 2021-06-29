3ac0bba73a91379b0faecae8c6547fc8b5602327
SophieTopart <sophiebailly9@gmail.com>
2021-06-29 10:56:12 +0200

    Start menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import '../style/home.scss'
 import BackgroundStars from './BackgroundStars'
+import SlideMenu from '../components/menu/Menu'
 
 export default function Home () {
   return(
   <div className="container">
     <BackgroundStars/>
+    <SlideMenu/>
     <div id="home">
       <h1>Pomodoro</h1>
       <div className="back">
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,135 @@
+import styled from "styled-components"
+import { useState, useRef } from "react"
+
+export const theme = {
+    primaryDark: '#0D0C1D',
+    primaryLight: '#EFFFFA',
+    primaryHover: '#343078',
+    mobile: '576px',
+  }
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #EFFFFA;
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  height: 100vh;
+  text-align: left;
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+
+  @media (max-width: 576px) {
+      width: 100%;
+    }
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #0D0C1D;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`
+
+const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <a href="/">
+        <span role="img" aria-label="about us">💁🏻‍♂️</span>
+        Work efficiently
+      </a>
+      <a href="/">
+        <span role="img" aria-label="price">💸</span>
+        Relax
+        </a>
+      <a href="/">
+        <span role="img" aria-label="contact">📩</span>
+        Fuel yourself
+        </a>
+    </StyledMenu>
+  )
+}
+
+const StyledBurger = styled.button`
+  position: absolute;
+  top: 5%;
+  left: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ open }) => open ? '#0D0C1D' : '#EFFFFA'};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => open ? '0' : '1'};
+      transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+    }
+  }
+`
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  )
+}
+
+
+export default function SlideMenu () {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  return (
+    <div>
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen} />
+      </div>
+    </div>
+  )  
+}
+
