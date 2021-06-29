c557f1f1063257acb4d8568f2aff282784c17e75
JaddN7 <jerome.shaaban@gmail.com>
2021-06-29 11:53:14 +0200

    Counter with quotes done

--- src/components/PomodoroMusic.jsx
+++ src/components/PomodoroMusic.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import '../style/pomodoroMusic.css';
 import BackgroundStars from './BackgroundStars';
 import logo from '../assets/25minwhite.png';
 import MusicPlayer from "./music_player/MusicPlayer"
+import Loader from './loader/Loader';
+import Pomodoro from './pomodoro/pomodoro';
+import '../style/pomodoroMusic.css'
 
 const PomodoroMusic = () => {
   return (
     <div>
       <BackgroundStars />
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" className="Logo"/>
       <MusicPlayer />
+      <div className="Container">
+        <Pomodoro />
+        <Loader />
+      </div>
     </div>
   );
 };
