7556612bb6bade62db3ef838a83462a766bb8cd4
Pierre <lightpower@hotmail.fr>
2021-06-29 12:11:02 +0200

    new component rest, for pomodoro rest phase, with button to go back on study phase

---
+++ src/components/loader/Rest.jsx
@@ -0,0 +1,22 @@
+import {useEffect} from 'react';
+import MillImg from '../../assets/coffee_mill.png'
+
+export default function ({setIsRest ,setTimer, pomodoroTimer}) {
+  useEffect(() => {
+    return () => {
+      setTimer(pomodoroTimer)
+    };
+  }, []);
+
+  const handleClick = () => {
+    setIsRest(false)
+  }
+return(
+  <>
+  <div id="mill">
+      <img src={MillImg} />
+  </div>
+      <button className="readyButton" onClick={() => {handleClick()}}>Ready</button>
+  </>)
+  
+}
