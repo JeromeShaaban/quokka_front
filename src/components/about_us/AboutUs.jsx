8111ba2e5f25064914109ead26b1dd722845114f
JaddN7 <jerome.shaaban@gmail.com>
2021-06-29 22:59:00 +0200

    Page About us done

--- src/components/about_us/AboutUs.jsx
+++ src/components/about_us/AboutUs.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import MemberCard from "./MemberCard";
+import Quokkanim from "./Quokkanim";
+import quokka1 from "../../assets/quokka_01.png";
+import quokka2 from "../../assets/quokka_02.png";
+import quokka3 from "../../assets/quokka_03.png";
+import quokka4 from "../../assets/quokka_04.png";
+import quokka5 from "../../assets/quokka_05.png";
 
 export default function AboutUs() {
-  const { open } = useContext(MenuContext);
   const members = [
     {
-      photo: '../../assets/quokka_5.png',
+      photo: quokka5,
       firstname: 'Violaine'
     },
   
     {
-      photo: '../../assets/quokka_4.png',
+      photo: quokka4,
       firstname: 'Pierre'
     },
   
     {
-      photo: '../../assets/quokka_3.png',
+      photo: quokka3,
       firstname: 'Sophie'
     },
   
     {
-      photo: '../../assets/quokka_2.png',
+      photo: quokka2,
       firstname: 'Jérôme'
     },
   
     {
-      photo: '../../assets/quokka_1.png',
+      photo: quokka1,
       firstname: 'Marie-Anne'
     },
   ]
@@ -34,14 +39,16 @@ export default function AboutUs() {
         <div className="CardDeck" 
         style={{
           display: "flex", 
-          flexDirection: "row", 
+          flexFlow: "row wrap", 
           width: "100%", 
-          justifyContent: "center", 
+          justifyContent: "space-evenly", 
           alignItems: "center"
         }}>
           {members.map((member) => {
             return(
-              <MemberCard key={member.firstname} photo={member.photo} firstname={member.firstname}/>
+              <Quokkanim rotation={5} timing={150}>
+                <MemberCard key={member.firstname} photo={member.photo} firstname={member.firstname}/>
+              </Quokkanim>
             )
           })
           }
