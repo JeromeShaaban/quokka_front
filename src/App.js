54dea048ec431b2897cdae9ae8a2803e825cd5ad
JaddN7 <jerome.shaaban@gmail.com>
2021-06-30 11:20:02 +0200

    Can size fixed + Github link on the cans + LinkedIn logo & link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import QuokkaRouter from "./Router";
 import './App.css';
-import MenuContext from './contexts/MenuContext'
+import MenuContext from './contexts/MenuContext';
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   const [open, setOpen] = useState(false);
