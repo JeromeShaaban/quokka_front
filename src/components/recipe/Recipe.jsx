66f72568a7f94f324991481df3487f7fcddab8d5
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-06-30 12:38:25 +0200

    crea form

--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -3,6 +3,8 @@ import Card from './Card';
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify';
+import { FormControl, InputLabel, Input} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 export const RecipeWrapper = styled.div`
 display:flex;
@@ -19,15 +21,24 @@ padding:50px;
 margin-left: auto;
 margin-right: auto;
 width: 16em;
-border-radius: 10%;
 font-size: 4rem;
+
+`;
+
+export const Form = styled.form`
+margin:0;
+background-color:#ffffff1c;
+padding:50px;
+margin-left: auto;
+margin-right: auto;
+width: 16em;
+font-size: 4rem;
+border-radius:5px;
 
 `;
 
 export default function Recipe(){
 
-
-    function Recipe() {
         const [recipes, setRecipes] = useState([])
         const [error, setError] = useState(null)
         const [loading, setLoading] = useState(true)
@@ -37,7 +48,7 @@ export default function Recipe(){
         useEffect(() => {
             const getRecipes= async () => {
               try {
-                const courses = await axios.get('https://twenty-five.herokuapp.com/')
+                const recipes = await axios.get('https://twenty-five.herokuapp.com/recipes')
                 setRecipes(recipes.data)
               } catch(err) {
                 setError(err)
@@ -56,7 +67,7 @@ export default function Recipe(){
             try {
               setLoading(true)
               await axios.post(
-                'https://twenty-five.herokuapp.com/',
+                'https://twenty-five.herokuapp.com/recipes',
                 {...datas}
               )
               toast.success('La recette à bien été rajouté !')
@@ -91,9 +102,89 @@ export default function Recipe(){
         }
                 </RecipeWrapper>
         </div>
-            <form onSubmit={handleSubmit}>
-                
-            </form>
+            <Form onSubmit={handleSubmit}>
+            <FormControl>
+              <InputLabel htmlFor="my-input">Title</InputLabel>
+              <Input
+                name="title"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">Image</InputLabel>
+              <Input
+                name="image"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">Ingredients</InputLabel>
+              <Input
+                name="ingredients"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">steps1</InputLabel>
+              <Input
+                name="steps1"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">steps2</InputLabel>
+              <Input
+                name="steps2"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">steps3</InputLabel>
+              <Input
+                name="steps3"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">steps4</InputLabel>
+              <Input
+                name="steps4"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="my-input">steps5</InputLabel>
+              <Input
+                name="steps5"
+                onChange={onChangeDatas}
+                id="my-input"
+                aria-describedby="my-helper-text"
+              />
+            </FormControl>
+            <Button
+              disabled={loading}
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+            </Form>
 
 
 
@@ -104,7 +195,7 @@ export default function Recipe(){
         )   
 
 
-        }
+        
 
 
        
